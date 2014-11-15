d8897a8bfbd18fa723df0b550de338b570b0beb1
youngsterxyf <sas.198708@gmail.com>
2014-11-15 22:44:26 +0800

    init

---
+++ scrapy_it/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_it project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_it'
+
+SPIDER_MODULES = ['scrapy_it.spiders']
+NEWSPIDER_MODULE = 'scrapy_it.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_it (+http://www.yourdomain.com)'
